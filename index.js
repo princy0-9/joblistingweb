8aaa7a1ba7ab2a249537911f92a261f7566c7459
princy0-9 <priyanka.singh2973@gmail.com>
2024-11-27 20:47:45 +0530

    added html file

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express")
+const path = require("path");
+
 const env = require("dotenv");
 env.config();
 const app = express();
 
 const PORT = process.env.PORT || 3001
 
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "try.html"));
+});
 app.listen(PORT, () => {
     console.log("Server is running on port 3001")
 
