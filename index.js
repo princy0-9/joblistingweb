03eac6f6da967855a0ff8e0eac28f50991ba88a7
princy0-9 <priyanka.singh2973@gmail.com>
2024-11-30 23:31:03 +0530

    register and login

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require("express")
 const path = require("path");
 const mongoose = require("mongoose")
-
 const env = require("dotenv");
+const userRouter = require("./routes/User");
 env.config();
 const app = express();
+app.use(express.json())
 
 const PORT = process.env.PORT || 3001
 
-app.use(express.static(path.join(__dirname, "public")));
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "try.html"));
-});
+app.use("/", userRouter)
 app.listen(PORT, () => {
     console.log("Server is running on port 3001")
     mongoose.connect(process.env.MONGODB_URI, {
---
+++ routes/User.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("../schema/userschema");
+const userRouter = express.Router();
+const jwt = require("jsonwebtoken");
+
+userRouter.post("/register", async (req,res) => {
+    const { name, email, password, mobileno} = req.body
+    const isUserExists = await User.findOne({ email})
+    if(isUserExists){
+        return res.send("User Already Exsits")
+    } 
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    try{
+        const user = await User({
+            name,
+            email,
+            password: hashPassword,
+            mobileno
+        })
+        const savedUser = await user.save();
+        res.json({ message: "User Added successfully!", data: savedUser });
+    } catch (err){
+        res.status(404).json({message: err.message})
+    }
+})
+
+userRouter.post("/login", async (req,res) => {
+    const { email, password} = req.body
+    const isUserExists = await User.findOne({ email})
+
+    try{
+        if(!isUserExists){
+            return res.send("Invalid Credentials")
+        } 
+
+        const isPasswordValid = await bcrypt.compare(password, isUserExists.password);
+        if(!isPasswordValid){
+            return res.send("Invalid Credentials")
+        } 
+        
+        const token = await jwt.sign({_id:isUserExists._id}, process.env.JWT_SECRET_TOKEN)
+        res.status(200).json({ token });
+    } catch (err){
+        res.status(404).json({message: err.message})
+    }
+})
+
+module.exports = userRouter;
